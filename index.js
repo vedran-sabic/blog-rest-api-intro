71aa51fc56ccd15a7a8e7df274955a10971f1b15
vedran-sabic <vedran.sabic@skole.hr>
2023-02-05 01:04:30 +0100

    form reset and refactor

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+let postsArray = [];
+const titleInput = document.getElementById("post-title");
+const bodyInput = document.getElementById("post-body");
+const form = document.getElementById("new-post");
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postsArr) {
-      html += `
-                <h3 class="blah">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
-    }
-    document.getElementById("blog-list").innerHTML = html;
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
-document.getElementById("new-post").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const postTitle = document.getElementById("post-title").value;
-  const postBody = document.getElementById("post-body").value;
+  const postTitle = titleInput.value;
+  const postBody = bodyInput.value;
   const data = {
     title: postTitle,
     body: postBody,
@@ -33,14 +42,13 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
   fetch("https://jsonplaceholder.typicode.com/posts", options)
     .then((res) => res.json())
     .then((post) => {
+      postsArray.unshift(post);
+      renderPosts();
       /**
-       * Challenge: Update the DOM with the new blog entry
+       * Challenge: clear the form out!
        */
-      document.getElementById("blog-list").innerHTML = `
-                <h3 class="blah">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-                ${document.getElementById("blog-list").innerHTML}
-            `;
+      titleInput.value = "";
+      bodyInput.value = "";
+      // form.reset()
     });
 });
